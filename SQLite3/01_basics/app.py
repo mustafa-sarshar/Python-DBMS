a963674e8b6d6ee06d8355f7ff902efa25d882d8
Mustafa Sarshar <mustafa.sarshar88@gmail.com>
2021-12-29 20:44:52 +0100

    update

--- SQLite3/01_basics/app.py
+++ SQLite3/01_basics/app.py
@@ -4,58 +4,60 @@ Inspired by:
 """
 # In[] Libs
 import sqlite3
+import os
 
 # In[] Inits
 DB_ADDRESS = "database.db"
 TABLE_NAME = "users"
+db_connection = None
 
 # In[] Functions
-def reset_db(db_address=""):
-    import os
-    if os.path.exists(db_address):
-        os.remove(db_address)
-
 def init_db(db_address="", table_name=""):
+    global db_connection
+    # Check if the database already exists or not
+    if os.path.exists(db_address):
+        os.remove(db_address) # remove the database if already exists
+
     # Creating a Connection object that represents the database
-    con = sqlite3.connect(db_address) # The special path name :memory: can be provided to create a temporary database in RAM.
+    db_connection = sqlite3.connect(db_address) # The special path name :memory: can be provided to create a temporary database in RAM.
 
     # Create users table
-    cur = con.cursor()
+    cur = db_connection.cursor()
     statement = f"""CREATE TABLE '{table_name}'
                     (fname text, lname text, gender text, age real)"""
     cur.execute(statement)
-    con.commit()
-    con.close()
 
-def add_user(db_address="", table_name="", fname="", lname="", gender="", age=0):
-    con = sqlite3.connect(db_address)
-    cur = con.cursor()    
+    db_connection.commit()
+
+def add_user(table_name="", fname="", lname="", gender="", age=0):
+    global db_connection
+    cur = db_connection.cursor()    
 
     # Insert a row of data
     statement = f"""INSERT INTO '{table_name}'
                     VALUES
                         ('{fname}', '{lname}', '{gender}', {age})"""
     cur.execute(statement)
-    con.commit()
-    con.close()
 
-def show_data(db_address="", table_name="", order_by=""):
-    con = sqlite3.connect(db_address)
-    cur = con.cursor()
+    db_connection.commit()
+
+def show_data(table_name="", order_by=""):
+    global db_connection
+    cur = db_connection.cursor()
     statement = f"""SELECT * FROM '{table_name}'
                     ORDER BY '{order_by}'"""
     for row in cur.execute(statement):
         print(row)
-    con.close()
 
 # In[] App
 if __name__ == "__main__":
-    reset_db(db_address=DB_ADDRESS)
+    
     init_db(db_address=DB_ADDRESS, table_name=TABLE_NAME)
-    add_user(db_address=DB_ADDRESS, table_name=TABLE_NAME, fname="Mustafa", lname="Sarshar", gender="male", age=33)
-    add_user(db_address=DB_ADDRESS, table_name=TABLE_NAME, fname="Anna", lname="Zieger", gender="female", age=21)
-    add_user(db_address=DB_ADDRESS, table_name=TABLE_NAME, fname="Yu", lname="Jang", gender="diverse", age=50)
+    add_user(table_name=TABLE_NAME, fname="Mustafa", lname="Sarshar", gender="male", age=33)
+    add_user(table_name=TABLE_NAME, fname="Anna", lname="Zieger", gender="female", age=21)
+    add_user(table_name=TABLE_NAME, fname="Yu", lname="Jang", gender="diverse", age=50)
     print("\nThese records are added to the database:\n")
-    show_data(db_address=DB_ADDRESS, table_name=TABLE_NAME, order_by="fname")
+    show_data(table_name=TABLE_NAME, order_by="fname")
 
     print("\nBye Bye!!!")
+    db_connection.close()
